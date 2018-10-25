52b15483db5144c575da80bc134f643ea9e036f5
Jeferson <jppmainini@gmail.com>
2018-10-25 18:03:01 -0300

    first commit

---
+++ resources/views/admin/cadastros/clientes/clientes.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.core.base')
+
+@section('title')
+    Clientes
+@endsection
+
+@section('main')
+<h1>Clientes</h1>
+@endsection
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('core.base')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+    <section class="invoice">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2 class="page-header">
+                    <i class="fa fa-globe"></i> AdminLTE, Inc.
+                    <small class="pull-right">Date: 2/10/2014</small>
+                </h2>
+            </div>
+            <!-- /.col -->
+        </div>
+    </section>
+@endsection

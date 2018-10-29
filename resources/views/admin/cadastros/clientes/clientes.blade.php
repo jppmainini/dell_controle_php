2aa53a37bd3cf1aaf83a9413260384edef7cbdbd
Jeferson <jppmainini@gmail.com>
2018-10-29 17:55:41 -0300

    Dellasta

--- resources/views/admin/cadastros/clientes/clientes.blade.php
+++ resources/views/admin/cadastros/clientes/clientes.blade.php
@@ -13,18 +13,15 @@
 @endsection
 
 @section('content')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                Data Tables
-                <small>advanced tables</small>
+                Cadastro de Clientes
+                <small>Lista de Clientes</small>
             </h1>
             <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li><a href="#">Tables</a></li>
-                <li class="active">Data tables</li>
+                <li><a href="/"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                <li class="active">Clientes</li>
             </ol>
         </section>
 
@@ -96,8 +93,6 @@
             </div>
             <!-- /.row -->
         </section>
-        <!-- /.content -->
-    </div>
     <!-- /.content-wrapper -->
 
     <!-- ./wrapper -->
